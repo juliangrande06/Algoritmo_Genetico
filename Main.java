fc472c5f655d1ae91ea0ecfbfa361468fc0dd23d
JG <jgrande@alumnos.exa.unicen.edu.ar>
2022-02-17 19:10:40 -0300

    Implementacion del Cruce Uniforme

--- Main.java
+++ Main.java
@@ -7,16 +7,23 @@ public class Main {
 	
     //Variables a considerar
     static int long_sec = 2;                        //Longitud de Secuencia
+    static int cant_parametros = 5;                 //< carga/descarga , bat_act , bat_obj , estado_CPU , estado_pant >
     
     static Integer estado_CPU[] = {0,30,50,75,100}; //Estado del CPU del dispositivo
     static Integer estado_pant[] = {0, 1};          //Estado de la pantalla
     
-    static int cant_pob = 10;                       //Cantidad de individuos de la poblacion
-    static double cant_ind_torneo = 5;              //Cantidad de individuos que compiten en un torneo
+    static int cant_pob = 10;                         //Cantidad de individuos de la poblacion
+    static double cant_ind_torneo = 5;                //Cantidad de individuos que compiten en un torneo
+    static double prob_cruce = 0.6;                   //Probabilidad de Cruce
+    static int tam_bloque = long_sec*cant_parametros; //Tamano del bloque para el Cruce Uniforme
+    static double prob_cruce_uniforme = 0.5;          //Probabilidad de Cruce Uniforme
+
     static ArrayList<ArrayList<Integer>> poblacion = new ArrayList<>(); //Poblacion de todas las soluciones
     static List<Integer> pob_padres = new ArrayList<>(); //Contenedor de los ID de los individuos elegidos en la seleccion de padres
+    static ArrayList<ArrayList<Integer>> pob_hijos = new ArrayList<>(); //Poblacion de todas las soluciones hijas
 
     //Datos de entrada
+    
     static int cant_mobil = 3;                                //Cantidad de dispositivos
     static String modelo[] = {"Xiaomi", "Motorola", "Samsung"}; //new String[cant_mobil];          //Modelo de dispositivo
     static Integer nivel_bat_act[] = {20, 30, 40}; //new Integer[cant_mobil]; //Nivel bateria actual
@@ -108,12 +115,56 @@ public class Main {
         }
     }
 
-    public static void mostrarPoblacion(){
+    public static void cruceUniforme(){
+        //System.out.println("\n   Cruce Uniforme");
+        double prob_aleatoria;
+        ArrayList<Integer> hijo1;
+        ArrayList<Integer> hijo2;
+
+        for(int i=0; i<cant_pob; i= i+2){
+            hijo1= new ArrayList<>(poblacion.get(i));
+            //System.out.println("");
+            //mostrarIndividuo(hijo1);
+            hijo2= new ArrayList<>(poblacion.get(i+1));
+            //mostrarIndividuo(hijo2);
+            prob_aleatoria= Math.random();
+
+            if(prob_aleatoria < prob_cruce){
+                for(int j=0; j<cant_mobil; j++){
+                    prob_aleatoria= Math.random();
+                    
+                    if(prob_aleatoria < prob_cruce_uniforme){
+                        int indice_intermedio= j*tam_bloque;
+                        for(int k=0; k<tam_bloque; k++){
+                            int valor_intermedio= hijo1.get(indice_intermedio);
+                            hijo1.set(indice_intermedio, hijo2.get(indice_intermedio));
+                            hijo2.set(indice_intermedio, valor_intermedio);
+                            indice_intermedio++;
+                        }
+                    }
+                }
+                //Limpio valores de Fitnes acarreado por los padres
+                int max_indice= cant_mobil*tam_bloque;
+                for(int j=max_indice; j<hijo1.size(); j++){
+                    hijo1.remove(j);
+                    hijo2.remove(j);
+                }
+
+                //System.out.println("\n### Hubo Cruce Uniforme");
+                //mostrarIndividuo(hijo1);
+                //mostrarIndividuo(hijo2);
+            }
+            pob_hijos.add(hijo1);
+            pob_hijos.add(hijo2);
+        }
+    }
+
+    public static void mostrarPoblacion(ArrayList<ArrayList<Integer>> pob){
         System.out.println("** Mostrando la Poblacion");
         for(int j=0; j<cant_pob; j++){
             System.out.println("* Solucion "+j);
-            for(int i=0; i<poblacion.get(j).size(); i++){
-            	System.out.print(poblacion.get(j).get(i) + ",");
+            for(int i=0; i<pob.get(j).size(); i++){
+            	System.out.print(pob.get(j).get(i) + ",");
             }
             System.out.println("");
         }
@@ -138,9 +189,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         inicializacionAleatoria();
-        //mostrarPoblacion();
+        //mostrarPoblacion(poblacion);
         fitness();
         seleccionPadres();
         //mostrarListaPadres();
+        cruceUniforme();
+        //mostrarPoblacion(pob_hijos);
     }
 }
